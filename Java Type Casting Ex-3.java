43917a7043cf23933ef430ee20b7d4c7a79e6c0e
Tumpa Sharma  Mitu <tumpasharmamitu@gmail.com>
2025-08-22 20:16:26 +0600

    Create Java Type Casting Ex-3.java

---
+++ Java Type Casting Ex-3.java
@@ -0,0 +1,18 @@
+public class main{
+
+public static void main(String args[]){  
+
+  
+int maxScore = 500;  
+
+ 
+int userScore = 423;  
+
+  
+float percentage = (float) userScore / maxScore * 100.0f;  
+
+System.out.println("User's percentage is " + percentage);
+
+}
+}
+

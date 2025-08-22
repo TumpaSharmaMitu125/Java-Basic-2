a21338a0b114b29214ff782171070a703f04d1f3
Tumpa Sharma  Mitu <tumpasharmamitu@gmail.com>
2025-08-22 20:15:03 +0600

    Create Java Type Casting Ex-2.java

---
+++ Java Type Casting Ex-2.java
@@ -0,0 +1,13 @@
+public class main{
+
+public static void main(String args[]){  
+
+double myDouble = 9.78d;  
+int myInt = (int) myDouble;   
+
+System.out.println(myDouble);  
+System.out.println(myInt);
+
+}
+}
+
